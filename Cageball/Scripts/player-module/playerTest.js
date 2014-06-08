851fac0a2e9fa29949af0750f168297a54999159
Martin Dzhonov <martin.dzhonov@gmail.com>
2014-06-08 15:58:27 +0300

    added player trail

--- Cageball/Scripts/player-module/playerTest.js
+++ Cageball/Scripts/player-module/playerTest.js
@@ -1,12 +1,22 @@
 ﻿(function () {
     var fieldWidth = 800;
     var fieldHeight = 600;
+    var playerX = 5;
+    var playerY = 5;
+    var playerRadius = 5;
+    var leftArrowCode = 37;
+    var rightArrowCode = 39;
+    var upArrowCode = 38;
+    var downArrowCode = 40;
+    var stepSize = 3;
+    var anim = new Kinetic.Animation();
 
     var stage = new Kinetic.Stage({
         container: "the-container",
         width: fieldWidth,
         height: fieldHeight,
     });
+
     var layer = new Kinetic.Layer();
 
     var field = new Kinetic.Rect({
@@ -16,76 +26,92 @@
         height: 600,
         fill: "black",
         stroke: 'blue',
-        strokeWidth: 21,
+        strokeWidth: 20,
     })
 
-    var playerX = 6;
-    var playerY = 6;
-    var playerRadius = 6;
-    var playerCircle = new Kinetic.Circle({
+    var player = new Kinetic.Circle({
         x: playerX,
         y: playerY,
         radius: playerRadius,
         fill: "red",
     })
 
-    layer.add(field);
-    layer.add(playerCircle);
-    stage.add(layer);
+    var trail = new Kinetic.Line({
+        points: [],
+        stroke: 'blue',
+        strokeWidth: 7,
+    });
 
-    var leftArrowCode = 37;
-    var rightArrowCode = 39;
-    var upArrowCode = 38;
-    var downArrowCode = 40;
-    var stepSize = 3;
-    var anim = new Kinetic.Animation();
     document.addEventListener('keydown', function (event) {
         anim.stop();
         if (event.keyCode == leftArrowCode) {
-            animateInDirection(playerCircle, -stepSize, 0);
+            animateInDirection(player, -stepSize, 0);
         }
         if (event.keyCode == rightArrowCode) {
-            animateInDirection(playerCircle, stepSize, 0);
+            animateInDirection(player, stepSize, 0);
         }
         if (event.keyCode == upArrowCode) {
-            animateInDirection(playerCircle, 0, -stepSize);
+            animateInDirection(player, 0, -stepSize);
         }
         if (event.keyCode == downArrowCode) {
-            animateInDirection(playerCircle, 0, stepSize);
+            animateInDirection(player, 0, stepSize);
         }
     })
-
 
     function animateInDirection(node, newX, newY) {
         anim = new Kinetic.Animation(function () {
             node.move({ x: newX, y: newY }, 0);
         }, layer);
-        stopAtPerimeter();
         anim.start();
     }
 
     function stopAtPerimeter() {
-        if (playerCircle.x() < playerRadius) {
+        if (player.x() < playerRadius) {
             anim.stop();
-            playerCircle.setPosition({ x: playerRadius, y: playerCircle.y() });
-            playerCircle.draw();
+            player.setPosition({ x: playerRadius, y: player.y() });
         }
-        if (playerCircle.y() < playerRadius) {
+        if (player.y() < playerRadius) {
             anim.stop();
-            playerCircle.setPosition({ x: playerCircle.x(), y: playerRadius });
-            playerCircle.draw();
+            player.setPosition({ x: player.x(), y: playerRadius });
         }
-        if (playerCircle.x() > fieldWidth - playerRadius) {
+        if (player.x() > fieldWidth - playerRadius) {
             anim.stop();
-            playerCircle.setPosition({ x: fieldWidth - playerRadius, y: playerCircle.y() });
-            playerCircle.draw();
+            player.setPosition({ x: fieldWidth - playerRadius, y: player.y() });
         }
-        if (playerCircle.y() > fieldHeight - playerRadius) {
+        if (player.y() > fieldHeight - playerRadius) {
             anim.stop();
-            playerCircle.setPosition({ x: playerCircle.x(), y: fieldHeight - playerRadius });
-            playerCircle.draw();
+            player.setPosition({ x: player.x(), y: fieldHeight - playerRadius });
+        }
+        player.draw();
+    }
+    window.setInterval(stopAtPerimeter, 100);
+
+    function drawTrail() {
+
+        if (playerIsOnPerimeter()) {
+            trail.attrs.points = [];
+        }
+        else {
+            trail.attrs.points.push(player.x());
+            trail.attrs.points.push(player.y());
+            layer.draw()
         }
     }
-    window.setInterval(stopAtPerimeter, 5);
+    window.setInterval(drawTrail, 30);
+
+    function playerIsOnPerimeter() {
+        if (player.x() == playerRadius || player.y() == playerRadius
+            || player.x() == fieldWidth - playerRadius
+            || player.y() == fieldHeight - playerRadius) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    layer.add(field);
+    layer.add(player);
+    layer.add(trail);
+    stage.add(layer);
     return stage;
 }());
