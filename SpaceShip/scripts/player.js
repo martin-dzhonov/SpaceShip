732224dbc8b5ddc6eda102ccf7c4de232d3c0e69
martin-dzhonov <martin.dzhonov@gmail.com>
2014-06-13 15:35:36 -0700

    Explosions working

--- SpaceShip/scripts/player.js
+++ SpaceShip/scripts/player.js
@@ -1,21 +1,26 @@
 ﻿//TODO: Refactor this file to reuse the code and to handle the stage outside.
 window.onload = function () {
     var screenWidth = 640,
-        screenHeight = 640,
-        playerShip,
+        screenHeight = 620;
+
+    var playerShip,
         playerStartPosX = screenWidth / 2,
         playerStartPosY = screenHeight - 50,
-        moveStep = 7,
-        enemies = [],
+        moveStep = 7;
+
+    var enemies = [],
         playerProjectiles = [],
         enemyProjectiles = [],
-        HUDLayer = new Kinetic.Layer(),
+        explosions = [];
+
+    var HUDLayer = new Kinetic.Layer(),
         playerLayer = new Kinetic.Layer(),
         projectileLayer = new Kinetic.Layer(),
         enemyLayer = new Kinetic.Layer(),
         enemyProjectileLayer = new Kinetic.Layer(),
         playerSpriteSheet = new Image(),
         enemySprite = new Image(),
+        starSkyImageObj = new Image(),
         score = 0,
         scoreText;
 
@@ -25,34 +30,33 @@ window.onload = function () {
         height: screenHeight
     });
 
-    playerSpriteSheet.onload = function () {
+    starSkyImageObj.onload = function () {
 
         initHUD();
         initPlayer();
 
-        executeMovementInput();
-        executeShootingInput();
+        window.addEventListener('keydown', executeMovementInput);
+        window.addEventListener('click', playerShoot);
 
-        stage.add(HUDLayer);
-        stage.add(enemyLayer);
-        stage.add(playerLayer);
-        stage.add(projectileLayer);
-        stage.add(enemyProjectileLayer);
+        addLayers();
+
+        var enemyGeneration = setInterval(generateEnemies, 1000);
+
+        var enemyShooting = setInterval(function () {
+            for (var i = 0; i < enemies.length; i++) {
+                enemyShoot(enemies[i]);
+            }
+        }, 3000);
+
+        var runGame = setInterval(update, 10);
 
         function update() {
             moveEnemies();
             destroyOutOfScreenProjectiles();
             hitEnemies();
             hitPlayer();
+            stopExplosions()
         }
-        
-        var runGame = setInterval(update, 10);
-        var enemyGeneration = setInterval(generateEnemies, 1000);
-        var enemyShooting = setInterval(function () {
-            for (var i = 0; i < enemies.length; i++) {
-                enemyShoot(enemies[i]);
-            }
-        }, 2000);
 
         function hitPlayer() {
             if (enemyProjectiles.length > 0) {
@@ -65,56 +69,38 @@ window.onload = function () {
                         (currentProjectyle.attrs.x <= playerShip.attrs.y + 42)) {
 
                         playerShip.animation('explode');
-
-                        //enemyProjectiles.length = 0; doesn't work
-                        //console.log(enemyProjectiles);
+                        window.removeEventListener('keydown', executeMovementInput);
+                        window.removeEventListener('click', playerShoot);
                         clearInterval(runGame);
                         clearInterval(enemyGeneration);
                         clearInterval(enemyShooting);
-
                     }
                 }
             }
         }
     };
 
-    function enemyShoot(enemy) {
-        var currentEnemyProjectile = new Kinetic.Sprite({
-            x: enemy.attrs.x + 20,
-            y: enemy.attrs.y,
-            image: playerSpriteSheet,
-            animation: 'simpleFire',
-            animations: {
-                simpleFire: [
-                    9, 131, 3, 7,
-                    4, 131, 3, 9
-                ],
-                rocket: [
-                    16, 131, 4, 7,
-                    24, 131, 4, 9,
-                    32, 131, 4, 11,
-                    40, 131, 4, 13,
-                    47, 131, 6, 15,
-                    56, 131, 4, 14,
-                    64, 131, 4, 12
-                ]
-            }
+    function initHUD() {
+
+        var starSky = new Kinetic.Image({
+            x: 0,
+            y: 0,
+            image: starSkyImageObj,
+            width: 640,
+            height: 640
+        });
+        HUDLayer.add(starSky);
+
+        scoreText = new Kinetic.Text({
+            x: 15,
+            y: 15,
+            text: '0',
+            fontSize: 20,
+            fontFamily: 'Calibri',
+            fill: 'yellow'
         });
 
-        enemyProjectiles.push(currentEnemyProjectile);
-        enemyProjectileLayer.add(currentEnemyProjectile);
-        currentEnemyProjectile.start();
-    }
-
-    function moveEnemies() {
-        for (var i = 0; i < enemies.length; i++) {
-            enemies[i].move({ x: 0, y: 1 });
-            if (enemies[i].y() > screenHeight) {
-                enemies[i].destroy();
-                enemies.splice(i, 1);
-            }
-        }
-        enemyLayer.draw();
+        HUDLayer.add(scoreText);
     }
 
     function initPlayer() {
@@ -169,62 +155,26 @@ window.onload = function () {
         playerLayer.add(playerShip);
         playerShip.start();
     }
-    function initHUD() {
-        var blackRect = new Kinetic.Rect({
-            width: screenWidth,
-            height: screenHeight,
-            fill: "black",
-        });
 
-        scoreText = new Kinetic.Text({
-            x: 15,
-            y: 15,
-            text: '0',
-            fontSize: 20,
-            fontFamily: 'Calibri',
-            fill: 'yellow'
-        });
-
-        HUDLayer.add(blackRect);
-        HUDLayer.add(scoreText);
-    }
-
-    function generateEnemies() {
-        var enemy = new Kinetic.Image({
-            x: Math.floor((Math.random() * (screenWidth - enemySprite.width))),
-            y: -enemySprite.height,
-            width: enemySprite.width,
-            heigth: enemySprite.height,
-            image: enemySprite,
-        });
-
-        enemyShoot(enemy);
-        enemies.push(enemy);
-        enemyLayer.add(enemy);
-    }
-
-    function executeMovementInput() {
-        window.addEventListener('keydown', function (ev) {
-            switch (ev.keyCode) {
-                case 37:
-                case 65:
-                    flyLeft();
-                    break;
-                case 38:
-                case 87:
-                    flyUp();
-                    break;
-                case 39:
-                case 68:
-                    flyRight();
-                    break;
-                case 40:
-                case 83:
-                    flyDown();
-                    break;
-            }
-        });
-
+    function executeMovementInput(ev) {
+        switch (ev.keyCode) {
+            case 37:
+            case 65:
+                flyLeft();
+                break;
+            case 38:
+            case 87:
+                flyUp();
+                break;
+            case 39:
+            case 68:
+                flyRight();
+                break;
+            case 40:
+            case 83:
+                flyDown();
+                break;
+        }
         function flyLeft() {
             playerShip.animation('flyLeft');
             playerShip.setX(playerShip.attrs.x -= moveStep);
@@ -257,48 +207,93 @@ window.onload = function () {
             }
         }
     }
-    function executeShootingInput() {
-        window.addEventListener('keyup', function (ev) {
-            switch (ev.keyCode) {
-                case 32:
-                    playerShoot();
-                    break;
-                default:
-                    playerShip.animation('idlePosition');
-                    break;
+
+    function playerShoot() {
+        var currentProjectile = new Kinetic.Sprite({
+            x: playerShip.attrs.x + 20,
+            y: playerShip.attrs.y,
+            image: playerSpriteSheet,
+            animation: 'rocket',
+            animations: {
+                simpleFire: [
+                    9, 131, 3, 7,
+                    4, 131, 3, 9
+                ],
+                rocket: [
+                    16, 131, 4, 7,
+                    24, 131, 4, 9,
+                    32, 131, 4, 11,
+                    40, 131, 4, 13,
+                    47, 131, 6, 15,
+                    56, 131, 4, 14,
+                    64, 131, 4, 12
+                ]
+            }
+        })
+        playerProjectiles.push(currentProjectile);
+        projectileLayer.add(currentProjectile);
+        currentProjectile.start();
+    }
+
+    function addLayers() {
+        stage.add(HUDLayer);
+        stage.add(enemyLayer);
+        stage.add(playerLayer);
+        stage.add(projectileLayer);
+        stage.add(enemyProjectileLayer);
+    }
+
+    function generateEnemies() {
+        var enemy = new Kinetic.Image({
+            x: Math.floor((Math.random() * (screenWidth - enemySprite.width))),
+            y: -enemySprite.height,
+            width: enemySprite.width,
+            heigth: enemySprite.height,
+            image: enemySprite,
+        });
+
+        enemyShoot(enemy);
+        enemies.push(enemy);
+        enemyLayer.add(enemy);
+    }
+
+    function enemyShoot(enemy) {
+        var currentEnemyProjectile = new Kinetic.Sprite({
+            x: enemy.attrs.x + 30,
+            y: enemy.attrs.y + 50,
+            image: playerSpriteSheet,
+            animation: 'simpleFire',
+            animations: {
+                simpleFire: [
+                    9, 131, 3, 7,
+                    4, 131, 3, 9
+                ],
+                rocket: [
+                    16, 131, 4, 7,
+                    24, 131, 4, 9,
+                    32, 131, 4, 11,
+                    40, 131, 4, 13,
+                    47, 131, 6, 15,
+                    56, 131, 4, 14,
+                    64, 131, 4, 12
+                ]
             }
         });
 
-        document.getElementById('body').addEventListener('click', function myfunction() {
-            playerShoot();
-        });
+        enemyProjectiles.push(currentEnemyProjectile);
+        enemyProjectileLayer.add(currentEnemyProjectile);
+        currentEnemyProjectile.start();
+    }
 
-        function playerShoot() {
-            var currentProjectile = new Kinetic.Sprite({
-                x: playerShip.attrs.x + 20,
-                y: playerShip.attrs.y,
-                image: playerSpriteSheet,
-                animation: 'rocket',
-                animations: {
-                    simpleFire: [
-                        9, 131, 3, 7,
-                        4, 131, 3, 9
-                    ],
-                    rocket: [
-                        16, 131, 4, 7,
-                        24, 131, 4, 9,
-                        32, 131, 4, 11,
-                        40, 131, 4, 13,
-                        47, 131, 6, 15,
-                        56, 131, 4, 14,
-                        64, 131, 4, 12
-                    ]
-                }
-            })
-            playerProjectiles.push(currentProjectile);
-            projectileLayer.add(currentProjectile);
-            currentProjectile.start();
+    function moveEnemies() {
+        for (var i = 0; i < enemies.length; i++) {
+            enemies[i].move({ x: 0, y: 1 });
+            if (enemies[i].y() > screenHeight) {
+                enemies[i].destroy();
+                enemies.splice(i, 1);
+            }
         }
+        enemyLayer.draw();
     }
 
     function destroyOutOfScreenProjectiles() {
@@ -306,16 +301,18 @@ window.onload = function () {
             for (var i = 0; i < playerProjectiles.length; i++) {
                 playerProjectiles[i].setY(playerProjectiles[i].attrs.y -= 5);
                 if (playerProjectiles[i].attrs.y < 0) {
+
                     playerProjectiles[i].destroy();
                     playerProjectiles.splice(i, 1);
                     projectileLayer.draw();
-                }              
+                }
             }
         }
         if (enemyProjectiles.length > 0) {
             for (var j = 0; j < enemyProjectiles.length; j++) {
                 enemyProjectiles[j].setY(enemyProjectiles[j].attrs.y += 5);
                 if (enemyProjectiles[j].attrs.y >= screenHeight) {
+
                     enemyProjectiles[j].destroy();
                     enemyProjectiles.splice(j, 1);
                     enemyProjectileLayer.draw();
@@ -324,6 +321,34 @@ window.onload = function () {
         }
     }
 
+    function playExplosionAt(x, y) {
+        var explosion = new Kinetic.Sprite({
+            x: x,
+            y: y,
+            image: playerSpriteSheet,
+            animation: 'explode',
+            animations: {
+                explode: [
+					18, 363, 44, 44,
+					103, 367, 33, 35,
+					19, 516, 35, 35,
+					97, 514, 39, 34,
+					177, 512, 40, 41,
+					17, 442, 45, 44,
+					93, 440, 56, 50,
+					6, 586, 62, 56,
+					81, 580, 70, 68,
+					159, 576, 76, 73
+                ]
+            },
+            frameRate: 10,
+        });
+
+
+        explosions.push(explosion);
+        projectileLayer.add(explosion);
+        explosion.start();
+    }
 
     function hitEnemies() {
         if (playerProjectiles.length > 0 && enemies.length > 0) {
@@ -331,14 +356,20 @@ window.onload = function () {
                 var currentProjectile = playerProjectiles[i];
                 for (var j = 0; j < enemies.length; j++) {
                     var currentEnemy = enemies[j];
+
+                    //collision detection
                     if (currentProjectile.x() >= currentEnemy.x()
                         && currentProjectile.x() < currentEnemy.x() + currentEnemy.width()
                         && currentProjectile.y() <= currentEnemy.y() + currentEnemy.height()) {
 
+                        playExplosionAt(currentEnemy.x(), currentEnemy.y());
+
+                        //remove destroyed enemy
                         enemies[j].destroy();
                         enemies.splice(j, 1);
                         enemyLayer.draw();
 
+                        //remove destroyed projectile
                         playerProjectiles[i].destroy();
                         playerProjectiles.splice(i, 1);
                         projectileLayer.draw();
@@ -349,12 +380,22 @@ window.onload = function () {
             }
         }
     }
+
+    function stopExplosions() {
+        for (var i = 0; i < explosions.length; i++) {
+            if (explosions[i].frameIndex() === 9) {
+                explosions[i].destroy();
+            }
+        }
+    }
+
     function updateScore() {
-        score += 10;
+        score += 150;
         scoreText.text(score);
         HUDLayer.draw();
-        console.log(scoreText.text());
     }
+
     playerSpriteSheet.src = 'images/player-sprite.png';
     enemySprite.src = 'images/enemy.gif';
+    starSkyImageObj.src = 'images/starry-sky.jpg';
 }
